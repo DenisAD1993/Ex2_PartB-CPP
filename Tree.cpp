2951dcecca729b9df3e6236aa4b426cf86b5d9c0
Denis Adamovich <48647969+DenisAD1993@users.noreply.github.com>
2019-04-02 22:29:53 +0300

    Add files via upload

---
+++ Tree.cpp
@@ -0,0 +1,99 @@
+#include "Tree.hpp"
+using namespace ariel;
+
+void Tree::destroy_tree(struct Node *leaf){
+	if(leaf != NULL){
+		destroy_tree(leaf->getLeft());
+		destroy_tree(leaf->getRight());
+		delete leaf;
+		leaf=NULL;
+	}
+}
+
+struct Node* Tree::insert(struct Node* leaf, int key){
+	if(leaf==NULL){
+		return new Node(key);
+	}else if(key < leaf->getValue()){
+		leaf->setLeft(insert(leaf->getLeft(), key));
+	}else if(key > leaf->getValue()){
+		leaf->setRight(insert(leaf->getRight(), key));
+	}else{
+		leaf=NULL;
+		throw std::invalid_argument("insert: This value already exists!");
+	}
+	return leaf;
+}
+
+void Tree::inorder_print(struct Node *leaf){
+	if(leaf != NULL){
+		inorder_print(leaf->getLeft());
+		cout << leaf->getValue() << " ";
+		inorder_print(leaf->getRight());
+	}
+}
+
+struct Node* Tree::FindMax(struct Node* leaf){
+	if(leaf==NULL)
+		return NULL;
+	while(leaf->getRight() != NULL){
+		leaf = leaf->getRight();
+	}
+	return leaf;
+}
+
+struct Node* Tree::remove(struct Node* leaf,int key){
+	if(leaf==NULL){
+		throw std::invalid_argument("remove: No such value to remove");
+		return NULL;
+	}
+	else if(key < leaf->getValue()) 
+		leaf->setLeft(remove(leaf->getLeft(), key));
+	else if (key > leaf->getValue())
+		leaf->setRight(remove(leaf->getRight(), key));
+	else{
+		if(leaf->getRight() == NULL && leaf->getLeft() == NULL){
+			delete leaf;
+			leaf=NULL;
+		}
+		else if(leaf->getRight() == NULL){
+			struct Node* temp=leaf->getLeft();
+			delete leaf;
+			return temp;
+		}
+
+		else if(leaf->getLeft() == NULL){
+			struct Node* temp =leaf->getRight();
+			delete leaf;
+			return temp;
+		}
+		else{
+			struct Node* temp = FindMax(leaf->getLeft());
+			leaf->setValue(temp->getValue());
+			leaf->setLeft(remove(leaf->getLeft(), temp->getValue()));
+		}
+	}
+	return leaf;
+}
+
+int Tree::size(struct Node* leaf){
+	if (leaf == NULL)
+		return 0;
+	else
+		return(size(leaf->getLeft()) + 1 + size(leaf->getRight()));
+}
+
+struct Node* Tree::FindLeaf(struct Node *leaf, int key){	
+	if ( leaf->getValue() > key && leaf->getLeft()!=NULL){
+		if(leaf->getLeft()->getValue()==key)
+			return leaf->getLeft();
+		else
+			return FindLeaf(leaf->getLeft(), key);
+	}else if (leaf->getValue() < key && leaf->getRight()!=NULL){
+		if(leaf->getRight()->getValue()==key)
+			return leaf->getRight();
+		else
+			return FindLeaf(leaf->getRight(), key);
+	}
+	return NULL;
+}
+
---
+++ Tree.hpp
@@ -0,0 +1,127 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+	private:
+		int value;
+		struct Node *left;
+		struct Node *right;
+	public:
+		Node(int val){value=val; right=NULL; left=NULL;}
+		Node(struct Node* l, struct Node* r, int val) {value=val; right=r; left=l;}
+	        
+		struct Node* getLeft() { return left;}
+		struct Node* getRight() {return right;}
+		int getValue(){return value;}
+
+		void setLeft(struct Node* l){left=l;}
+		void setRight(struct Node* r){right=r;}
+		void setValue(int val){value=val;}
+};
+
+
+namespace ariel
+{
+	class Tree{
+		
+		struct Node *Troot;
+		
+		public:
+		
+		Tree() {
+			Troot=NULL;
+		}
+		
+		~Tree() {
+			destroy_tree(Troot);
+		}
+		
+		void insert(int key) {  
+			Troot=insert(Troot, key);
+		}
+		
+		void print() {
+			if(Troot!=NULL) 
+				inorder_print(Troot);	
+			else 
+				cout<<"print: empty";
+			cout<<endl;
+		}
+		
+		void remove(int key) {
+			Troot=remove(Troot,key);
+		}
+
+		bool contains(int key) {
+			if(Troot!=NULL){
+				if(Troot->getValue()==key)
+					return true;
+				else if(FindLeaf(Troot, key) !=NULL)
+					return true;
+			}
+			return false;
+		}
+		
+		int size() {
+			return size(Troot);
+		}
+		
+		int root() {
+			if(Troot!=NULL) 
+				return Troot -> getValue(); 
+			throw std::invalid_argument ("root: tree is empty");
+		}
+		
+		int parent(int key) {
+			if(Troot->getValue()==key) 
+				throw std::invalid_argument("parent: root has no parent");
+			else {
+				struct Node* temp=Troot;
+				while (temp!=NULL){
+					if(key > temp->getValue()){
+						if(temp->getRight()->getValue()==key)
+							return temp->getValue();
+						temp=temp->getRight();
+					}else{
+						if(temp->getLeft()->getValue()==key)
+							return temp->getValue();
+						temp=temp->getLeft();
+					}
+				}
+				throw std::invalid_argument("parent: no such value in tree");
+			}
+		}
+		
+		int left(int key) {
+			if(Troot->getValue()==key) 
+				return Troot->getLeft()->getValue();
+			else if(FindLeaf(Troot, key)==NULL || FindLeaf(Troot,key)->getLeft()==NULL) 
+				throw std::invalid_argument("left: no such value"); 
+			else 
+				return FindLeaf(Troot,key)-> getLeft() -> getValue();
+		}
+		
+		int right(int key) {
+			if (Troot->getValue()==key) 
+				return Troot->getRight()->getValue(); 
+			else if(FindLeaf(Troot, key)==NULL || FindLeaf(Troot, key) -> getRight()==NULL) 
+				throw std::invalid_argument("right: no such value"); 
+			else 
+				return FindLeaf(Troot, key) -> getRight() -> getValue();
+		}
+
+
+
+
+		private:
+						    	
+		void destroy_tree(struct Node *leaf);
+		struct Node* insert(struct Node *leaf, int key);
+		void inorder_print(struct Node *leaf);
+		struct Node* FindMax(struct Node* leaf);
+		struct Node* remove(struct Node *leaf,int key);
+		int size(struct Node* leaf);
+		struct Node* FindLeaf(struct Node *leaf, int key);
+	};
+}
